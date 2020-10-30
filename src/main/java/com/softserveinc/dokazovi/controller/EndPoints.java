8378d1167195b9f028387bc2a7b1f8499e05f27c
Ruslan Ponomarenko <panruslan@gmail.com>
2020-10-30 14:54:54 +0200

    first init commit

---
+++ src/main/java/com/softserveinc/dokazovi/controller/EndPoints.java
@@ -0,0 +1,10 @@
+package com.softserveinc.dokazovi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public final class EndPoints {
+
+	public static final String WEB_API = "/api";
+	public static final String VERSION = "/version";
+
+}
