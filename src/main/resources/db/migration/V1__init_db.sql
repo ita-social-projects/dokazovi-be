c50658f4f3e64f7a87bd668de8b1fb671394dc57
FHToE <m.oleinik89@gmail.com>
2020-11-03 18:38:06 +0200

    create db structure in migration

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -13,18 +13,207 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
+CREATE TYPE public.post_status AS ENUM (
+  'draft',
+  'moderation_first_sign',
+  'moderation_second_sign',
+  'published',
+  'archived'
+);
+
+ALTER TYPE public.post_status OWNER TO dokazovi;
+
+CREATE TYPE public.source_type AS ENUM (
+  'profile_image',
+  'post_image',
+  'post_video'
+);
+
+ALTER TYPE public.source_type OWNER TO dokazovi;
+
+CREATE TYPE public.user_status AS ENUM (
+  'new',
+  'active',
+  'deleted'
+);
+
+ALTER TYPE public.user_status OWNER TO dokazovi;
+
 CREATE TABLE public.users (
-    id integer NOT NULL,
-    created_at timestamp without time zone,
-    name character varying(255),
-    email character varying(255),
-    password character varying(255),
-    status character varying(255),
-    first_name character varying(255),
-    last_name character varying(255),
-    qualification character varying(255),
-    phone character varying(255),
-    biography text
+  "user_id" SERIAL PRIMARY KEY,
+  "email" varchar,
+  "password" varchar,
+  "status" public.user_status,
+  "first_name" varchar,
+  "last_name" varchar,
+  "qualification" varchar,
+  "phone" varchar,
+  "bio" text,
+  "created_at" TIMESTAMP DEFAULT (now())
 );
 
 ALTER TABLE public.users OWNER TO dokazovi;
+
+CREATE TABLE public.roles (
+  "role_id" SERIAL PRIMARY KEY,
+  "role_name" varchar
+);
+
+ALTER TABLE public.roles OWNER TO dokazovi;
+
+CREATE TABLE public.roles_users (
+  "role_id" int,
+  "user_id" int
+);
+
+ALTER TABLE public.roles_users OWNER TO dokazovi;
+
+CREATE TABLE public.regions (
+  "region_id" SERIAL PRIMARY KEY,
+  "name" varchar
+);
+
+ALTER TABLE public.regions OWNER TO dokazovi;
+
+CREATE TABLE public.institutions (
+  "institution_id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "region_id" int,
+  "address" varchar
+);
+
+ALTER TABLE public.institutions OWNER TO dokazovi;
+
+CREATE TABLE public.users_institutions (
+  "user_id" int,
+  "institution_id" int,
+  "is_primary" boolean
+);
+
+ALTER TABLE public.users_institutions OWNER TO dokazovi;
+
+CREATE TABLE public.tags (
+  "tag_id" SERIAL PRIMARY KEY,
+  "tag" varchar
+);
+
+ALTER TABLE public.tags OWNER TO dokazovi;
+
+CREATE TABLE public.posts_tags (
+  "post_id" int,
+  "tag_id" int
+);
+
+ALTER TABLE public.posts_tags OWNER TO dokazovi;
+
+CREATE TABLE public.posts (
+  "post_id" SERIAL PRIMARY KEY,
+  "author_id" int,
+  "direction_id" int,
+  "type_id" int,
+  "title" varchar,
+  "content" text,
+  "status" public.post_status,
+  "important" boolean,
+  "tags" varchar,
+  "created_at" TIMESTAMP DEFAULT (now()),
+  "modified_at" TIMESTAMP DEFAULT (now())
+);
+
+ALTER TABLE public.posts OWNER TO dokazovi;
+
+CREATE TABLE public.charities (
+  "charity_id" SERIAL PRIMARY KEY,
+  "body" text,
+  "author_id" INT,
+  "created_at" TIMESTAMP DEFAULT (now()),
+  "modified_at" TIMESTAMP DEFAULT (now())
+);
+
+ALTER TABLE public.charities OWNER TO dokazovi;
+
+CREATE TABLE public.sources (
+  "source_id" SERIAL PRIMARY KEY,
+  "type" public.source_type,
+  "value" varchar
+);
+
+ALTER TABLE public.sources OWNER TO dokazovi;
+
+CREATE TABLE public.users_sources (
+  "user_id" int,
+  "source_id" int
+);
+
+ALTER TABLE public.users_sources OWNER TO dokazovi;
+
+CREATE TABLE public.posts_sources (
+  "post_id" int,
+  "source_id" int
+);
+
+ALTER TABLE public.posts_sources OWNER TO dokazovi;
+
+CREATE TABLE public.users_directions (
+  "user_id" int,
+  "direction_id" int
+);
+
+ALTER TABLE public.users_directions OWNER TO dokazovi;
+
+CREATE TABLE public.directions (
+  "direction_id" SERIAL PRIMARY KEY,
+  "name" varchar
+);
+
+ALTER TABLE public.directions OWNER TO dokazovi;
+
+CREATE TABLE public.posts_directions (
+  "post_id" int,
+  "direction_id" int
+);
+
+ALTER TABLE public.posts_directions OWNER TO dokazovi;
+
+CREATE TABLE public.post_types (
+  "type_id" SERIAL PRIMARY KEY,
+  "name" varchar
+);
+
+ALTER TABLE public.post_types OWNER TO dokazovi;
+
+ALTER TABLE public.roles_users ADD FOREIGN KEY (role_id) REFERENCES public.roles(role_id);
+
+ALTER TABLE public.roles_users ADD FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+ALTER TABLE public.institutions ADD FOREIGN KEY (region_id) REFERENCES public.regions(region_id);
+
+ALTER TABLE public.users_institutions ADD FOREIGN KEY (user_id) REFERENCES public.users (user_id);
+
+ALTER TABLE public.users_institutions ADD FOREIGN KEY (institution_id) REFERENCES public.institutions (institution_id);
+
+ALTER TABLE public.posts_tags ADD FOREIGN KEY (post_id) REFERENCES public.posts (post_id);
+
+ALTER TABLE public.posts_tags ADD FOREIGN KEY (tag_id) REFERENCES public.tags (tag_id);
+
+ALTER TABLE public.posts ADD FOREIGN KEY (author_id) REFERENCES public.users (user_id);
+
+ALTER TABLE public.posts ADD FOREIGN KEY (type_id) REFERENCES public.post_types (type_id);
+
+ALTER TABLE public.charities ADD FOREIGN KEY (author_id) REFERENCES public.users (user_id);
+
+ALTER TABLE public.users_sources ADD FOREIGN KEY (user_id) REFERENCES public.users (user_id);
+
+ALTER TABLE public.users_sources ADD FOREIGN KEY (source_id) REFERENCES public.sources (source_id);
+
+ALTER TABLE public.posts_sources ADD FOREIGN KEY (post_id) REFERENCES public.posts (post_id);
+
+ALTER TABLE public.posts_sources ADD FOREIGN KEY (source_id) REFERENCES public.sources (source_id);
+
+ALTER TABLE public.users_directions ADD FOREIGN KEY (user_id) REFERENCES public.users (user_id);
+
+ALTER TABLE public.users_directions ADD FOREIGN KEY (direction_id) REFERENCES public.directions (direction_id);
+
+ALTER TABLE public.posts_directions ADD FOREIGN KEY (post_id) REFERENCES public.posts (post_id);
+
+ALTER TABLE public.posts_directions ADD FOREIGN KEY (direction_id) REFERENCES public.directions (direction_id);
