96702286c8948526438f7837d6081505923db394
Ruslan Ponomarenko <panruslan@gmail.com>
2020-11-04 02:17:44 +0200

    return back JpaRepository

--- src/main/java/com/softserveinc/dokazovi/repositories/PostRepository.java
+++ src/main/java/com/softserveinc/dokazovi/repositories/PostRepository.java
@@ -1,10 +1,10 @@
 package com.softserveinc.dokazovi.repositories;
 
 import com.softserveinc.dokazovi.entity.Post;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends PagingAndSortingRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
 
 }
