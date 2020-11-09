524607cba5871406f9f4b6a5800b019384791a20
brmdm <N12c345hjkokiaf>
2020-11-09 23:32:18 +0200

    Changed SpringSecurity config class for REST API. Added UserService and UserServiceImpl classes.
    Added method findByEmail() to UserRepository

--- src/main/java/com/softserveinc/dokazovi/controller/EndPoints.java
+++ src/main/java/com/softserveinc/dokazovi/controller/EndPoints.java
@@ -4,7 +4,4 @@ public final class EndPoints {
 
 	public static final String WEB_API = "/api";
 	public static final String VERSION = "/version";
-	public static final String REGISTRATION = "/registration";
-	public static final String LOGIN = "/login";
-
 }
