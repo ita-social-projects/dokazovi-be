f77e84adf5e184324426076824e02ffcb3e7317c
Ruslan Ponomarenko <panruslan@gmail.com>
2020-11-10 19:04:53 +0200

    delete argument in "/latest" endpoint

--- src/main/java/com/softserveinc/dokazovi/controller/PostController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/PostController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,10 +27,9 @@ public class PostController {
 	@ApiPageable
 	@GetMapping("/latest")
 	public ResponseEntity<Page<LatestPostDTO>> findLatestByStatus(
-			@RequestParam PostStatus postStatus,
 			@PageableDefault(sort = {"createdAt"}, direction = Sort.Direction.DESC) Pageable pageable) {
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(postService.findAllByStatus(postStatus, pageable));
+				.body(postService.findAllByStatus(PostStatus.PUBLISHED, pageable));
 	}
 }
