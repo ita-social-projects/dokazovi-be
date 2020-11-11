e3aa80a4054c2af4f20021d43217028bac0cae0c
AntonMishyn <mishinaa333@gmail.com>
2020-11-11 19:25:52 +0200

    Corrected mapping

--- src/main/java/com/softserveinc/dokazovi/mapper/InstitutionMapper.java
+++ src/main/java/com/softserveinc/dokazovi/mapper/InstitutionMapper.java
@@ -1,14 +1,15 @@
 package com.softserveinc.dokazovi.mapper;
 
 import com.softserveinc.dokazovi.dto.post.PostUserInstitutionDTO;
+import com.softserveinc.dokazovi.dto.user.ExpertInstitutionDTO;
 import com.softserveinc.dokazovi.entity.InstitutionEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {RegionMapper.class})
 public interface InstitutionMapper {
 	@Mapping(target = "region", source = "institution.region.name")
 	PostUserInstitutionDTO toPostUserInstitutionDTO (InstitutionEntity institution);
 
-
+	ExpertInstitutionDTO toExpertInstitutionDTO (InstitutionEntity institution);
 }
--- src/main/java/com/softserveinc/dokazovi/mapper/UserMapper.java
+++ src/main/java/com/softserveinc/dokazovi/mapper/UserMapper.java
@@ -6,12 +6,14 @@ import com.softserveinc.dokazovi.entity.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {InstitutionMapper.class})
+@Mapper(componentModel = "spring", uses = {InstitutionMapper.class, DirectionMapper.class})
 public interface UserMapper {
 
 	@Mapping(target = "institution", source = "mainInstitution")
 	PostUserDTO toPostUserDTO(UserEntity userEntity);
 
+	@Mapping(target = "institution", source = "mainInstitution")
+	@Mapping(target = "direction", source = "mainDirection")
 	ExpertPreviewDTO toExpertPreviewDTO(UserEntity userEntity);
 
 }
