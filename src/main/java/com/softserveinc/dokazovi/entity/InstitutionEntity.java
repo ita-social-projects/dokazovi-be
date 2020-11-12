e3938fc8a2911b0527c8ef1cb79c9e3e2cae5e6d
Anton Mishyn <45338450+AntonMishyn@users.noreply.github.com>
2020-11-12 16:34:15 +0200

    Feature/doctor preview (#41)
    * Created DTO for doctor preview, extended userEntity with avatar url.
    
    * Added DB migration
    
    * Minor tweaks
    
    * Corrected mapping
    
    * Migration implemented, controller, dto, mapping, repository
    
    Co-authored-by: Ruslan Ponomarenko <panruslan@gmail.com>

--- src/main/java/com/softserveinc/dokazovi/entity/InstitutionEntity.java
+++ src/main/java/com/softserveinc/dokazovi/entity/InstitutionEntity.java
@@ -5,13 +5,13 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -40,14 +40,15 @@ public class InstitutionEntity implements Serializable {
 	@Column(name = "address", nullable = false)
 	private String address;
 
-	@OneToMany(
-			cascade = {CascadeType.REFRESH, CascadeType.MERGE},
-			mappedBy = "institution",
-			orphanRemoval = true
-	)
+	@OneToMany(mappedBy = "mainInstitution")
 	@EqualsAndHashCode.Exclude
 	@ToString.Exclude
-	private Set<UserInstitutionEntity> users = new HashSet<>();
+	private Set<UserEntity> mainUsersInstitution = new HashSet<>();
+
+	@ManyToMany(mappedBy = "institutions")
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	Set<UserEntity> users = new HashSet<>();
 
 	public InstitutionEntity(RegionEntity region, String name, String address) {
 		this.name = name;
