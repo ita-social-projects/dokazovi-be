f788928d02c658e14b9025f00372a5de96172e86
brmdm <N12c345hjkokiaf>
2020-11-12 17:18:39 +0200

    Import modifying

--- src/main/java/com/softserveinc/dokazovi/controller/PostController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/PostController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
