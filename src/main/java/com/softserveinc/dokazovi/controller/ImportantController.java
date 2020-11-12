09152aee86ef2589f9c128d4c518c9b91aa2b65f
Nick Underhill <m.podopryhora@gmail.com>
2020-11-12 12:55:37 +0200

    hofix Project.xml

--- src/main/java/com/softserveinc/dokazovi/controller/ImportantController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/ImportantController.java
@@ -12,7 +12,6 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
