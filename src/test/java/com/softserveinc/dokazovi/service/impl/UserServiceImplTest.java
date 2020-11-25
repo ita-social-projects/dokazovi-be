c547d58b75ace2e399dc6c0a159e7fbf314894ee
AntonMishyn <mishinaa333@gmail.com>
2020-11-25 11:42:56 +0200

    Implemented unit tests

--- src/test/java/com/softserveinc/dokazovi/service/impl/UserServiceImplTest.java
+++ src/test/java/com/softserveinc/dokazovi/service/impl/UserServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.softserveinc.dokazovi.service.impl;
 
+import com.softserveinc.dokazovi.dto.user.RandomExpertFilteringDTO;
 import com.softserveinc.dokazovi.entity.UserEntity;
 import com.softserveinc.dokazovi.mapper.UserMapper;
-import com.softserveinc.dokazovi.dto.user.RandomExpertFilteringDTO;
 import com.softserveinc.dokazovi.repositories.UserRepository;
+import org.assertj.core.util.Sets;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -37,7 +41,21 @@ class UserServiceImplTest {
 		Page<UserEntity> userEntityPage = new PageImpl<>(List.of(new UserEntity(), new UserEntity()));
 		RandomExpertFilteringDTO requestBody = new RandomExpertFilteringDTO();
 
-		when(userRepository.findRandomActiveUsers(any(Pageable.class))).thenReturn(userEntityPage);
+		when(userRepository.findRandomActiveUsers(any(Pageable.class)))
+				.thenReturn(userEntityPage);
+		userService.getRandomExpertPreview(pageable, requestBody);
+
+		verify(userMapper, times(userEntityPage.getNumberOfElements())).toExpertPreviewDTO(any(UserEntity.class));
+	}
+
+	@Test
+	void getRandomExpertPreviewByDirections() {
+		Page<UserEntity> userEntityPage = new PageImpl<>(List.of(new UserEntity(), new UserEntity()));
+		RandomExpertFilteringDTO requestBody = new RandomExpertFilteringDTO();
+		requestBody.setDirectionsIds(Sets.newHashSet(Arrays.asList(1, 2)));
+
+		when(userRepository.findRandomActiveUsersByDirections(any(Pageable.class), ArgumentMatchers.anySet()))
+				.thenReturn(userEntityPage);
 		userService.getRandomExpertPreview(pageable, requestBody);
 
 		verify(userMapper, times(userEntityPage.getNumberOfElements())).toExpertPreviewDTO(any(UserEntity.class));
