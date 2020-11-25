9f746bdb4ed42ecc856af96a3857fadf2b65af9a
Ruslan Ponomarenko <panruslan@gmail.com>
2020-11-25 11:37:26 +0200

    fix TagServiceImplTest

--- src/test/java/com/softserveinc/dokazovi/service/impl/TagServiceImplTest.java
+++ src/test/java/com/softserveinc/dokazovi/service/impl/TagServiceImplTest.java
@@ -13,7 +13,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
