ac8de7bdcddca41db3fe2b4bc26fe6c6af4541eb
brmdm <60578980+brmdm@users.noreply.github.com>
2020-11-26 13:03:35 +0200

    Change codeStyle of PostServiceImplTest

--- src/test/java/com/softserveinc/dokazovi/service/impl/PostServiceImplTest.java
+++ src/test/java/com/softserveinc/dokazovi/service/impl/PostServiceImplTest.java
@@ -16,7 +16,9 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -65,8 +67,7 @@ class PostServiceImplTest {
 		Page<PostEntity> postEntityPage = new PageImpl<>(List.of(new PostEntity(), new PostEntity()));
 		Integer directionId = 1;
 		Integer typeId = 1;
-		when(postRepository.findAllByMainDirectionIdAndTypeId(any(Integer.class)
-				, any(Integer.class), any(Pageable.class)))
+		when(postRepository.findAllByMainDirectionIdAndTypeId(any(Integer.class), any(Integer.class), any(Pageable.class)))
 				.thenReturn(postEntityPage);
 		postService.findAllByMainDirection(directionId, typeId, null, pageable);
 		verify(postMapper, times(postEntityPage.getNumberOfElements())).toLatestPostDTO(any(PostEntity.class));
@@ -77,8 +78,7 @@ class PostServiceImplTest {
 		Page<PostEntity> postEntityPage = new PageImpl<>(List.of(new PostEntity(), new PostEntity()));
 		Integer directionId = 1;
 		Set<Integer> sets = Set.of(1, 2, 3, 4);
-		when(postRepository.findAllByMainDirectionIdAndTagsIdIn(any(Integer.class)
-				, anySet(), any(Pageable.class)))
+		when(postRepository.findAllByMainDirectionIdAndTagsIdIn(any(Integer.class), anySet(), any(Pageable.class)))
 				.thenReturn(postEntityPage);
 		postService.findAllByMainDirection(directionId, null, sets, pageable);
 		verify(postMapper, times(postEntityPage.getNumberOfElements())).toLatestPostDTO(any(PostEntity.class));
@@ -90,8 +90,7 @@ class PostServiceImplTest {
 		Integer directionId = 1;
 		Integer typeId = 1;
 		Set<Integer> sets = Set.of(1, 2, 3, 4);
-		when(postRepository.findAllByMainDirectionIdAndTypeIdAndTagsIdIn(any(Integer.class)
-				, any(Integer.class), anySet(), any(Pageable.class)))
+		when(postRepository.findAllByMainDirectionIdAndTypeIdAndTagsIdIn(any(Integer.class), any(Integer.class), anySet(), any(Pageable.class)))
 				.thenReturn(postEntityPage);
 		postService.findAllByMainDirection(directionId, typeId, sets, pageable);
 		verify(postMapper, times(postEntityPage.getNumberOfElements())).toLatestPostDTO(any(PostEntity.class));
