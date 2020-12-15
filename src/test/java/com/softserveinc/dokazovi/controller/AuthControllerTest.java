1a458ec058e17388383438a145912e773e0c249c
brmdm <60578980+brmdm@users.noreply.github.com>
2020-12-15 12:25:28 +0200

    test: add methods to AuthControllerTest

--- src/test/java/com/softserveinc/dokazovi/controller/AuthControllerTest.java
+++ src/test/java/com/softserveinc/dokazovi/controller/AuthControllerTest.java
@@ -1,7 +1,15 @@
 package com.softserveinc.dokazovi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softserveinc.dokazovi.entity.ProviderEntity;
+import com.softserveinc.dokazovi.entity.UserEntity;
 import com.softserveinc.dokazovi.entity.VerificationToken;
+import com.softserveinc.dokazovi.payload.LoginRequest;
+import com.softserveinc.dokazovi.payload.SignUpRequest;
+import com.softserveinc.dokazovi.security.TokenProvider;
+import com.softserveinc.dokazovi.service.ProviderService;
 import com.softserveinc.dokazovi.service.UserService;
+import com.softserveinc.dokazovi.util.MailSenderUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,13 +19,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Optional;
+
 import static com.softserveinc.dokazovi.controller.EndPoints.AUTH;
+import static com.softserveinc.dokazovi.controller.EndPoints.AUTH_LOGIN;
+import static com.softserveinc.dokazovi.controller.EndPoints.AUTH_SIGNUP;
 import static com.softserveinc.dokazovi.controller.EndPoints.AUTH_VERIFICATION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,7 +48,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AuthControllerTest {
 
     private MockMvc mockMvc;
-
+    @Mock
+    private TokenProvider tokenProvider;
+    @Mock
+    private AuthenticationManager authenticationManager;
+    @Mock
+    private MailSenderUtil mailSenderUtil;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private ProviderService providerService;
     @Mock
     private UserService userService;
     @InjectMocks
@@ -43,6 +72,64 @@ public class AuthControllerTest {
     }
 
     @Test
+    void loginUser() throws Exception {
+        String token = "950c9760-805e-449c-a966-2d0d5ebd86f4";
+        String email = "user@mail.com";
+        String password = "user";
+        String uri = AUTH + AUTH_LOGIN;
+        UserEntity user = UserEntity.builder()
+                .email(email)
+                .password(password)
+                .enabled(true)
+                .build();
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail(email);
+        loginRequest.setPassword(password);
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authentication);
+        when(tokenProvider.createToken(any(Authentication.class))).thenReturn(token);
+        when(userService.findByEmail(anyString())).thenReturn(user);
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(asJsonString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void registerUser() throws Exception {
+        String email = "user@mail.com";
+        String password = "user";
+        String uri = AUTH + AUTH_SIGNUP;
+        UserEntity user = UserEntity.builder()
+                .email(email)
+                .firstName("user")
+                .password(password)
+                .build();
+        SignUpRequest request = new SignUpRequest();
+        request.setEmail(user.getEmail());
+        request.setName(user.getFirstName());
+        request.setPassword(user.getPassword());
+        Optional<ProviderEntity> providerEntity = Optional.ofNullable(ProviderEntity.builder().build());
+        when(userService.saveUser(any(UserEntity.class))).thenReturn(user);
+        doNothing().when(mailSenderUtil).sendMessage(any(UserEntity.class));
+        when(passwordEncoder.encode(anyString())).thenReturn(password);
+        when(providerService.createLocalProviderEntityForUser(any(UserEntity.class), anyString()))
+                .thenReturn(providerEntity);
+        mockMvc.perform(MockMvcRequestBuilders.post(uri)
+                .content(asJsonString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+        assertEquals(email, user.getEmail());
+    }
+
+    @Test
     void registrationComplete() throws Exception {
         String token = "950c9760-805e-449c-a966-2d0d5ebd86f4";
         String uri = AUTH + AUTH_VERIFICATION + "?token=" + token;
@@ -53,5 +140,13 @@ public class AuthControllerTest {
         mockMvc.perform(get(uri)).andExpect(status().isOk());
         verify(userService).getVerificationToken(token);
         assertEquals(token, verificationToken.getToken());
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
