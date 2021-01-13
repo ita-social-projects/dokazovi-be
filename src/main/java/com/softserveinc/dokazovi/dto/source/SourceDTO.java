db02d6b9ad5d0a15427b0deebdc19397c7bd49db
brmdm <60578980+brmdm@users.noreply.github.com>
2021-01-13 11:54:51 +0200

    feat: add validation of dto for post request

--- src/main/java/com/softserveinc/dokazovi/dto/source/SourceDTO.java
+++ src/main/java/com/softserveinc/dokazovi/dto/source/SourceDTO.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SourceDTO {
+
+	@NotNull(message = "Source id are required")
 	private Integer id;
+
+	@NotBlank(message = "Sources value cannot be empty")
 	private String value;
 }
--- src/main/java/com/softserveinc/dokazovi/dto/tag/TagDTO.java
+++ src/main/java/com/softserveinc/dokazovi/dto/tag/TagDTO.java
@@ -5,11 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TagDTO {
+
+	@NotNull(message = "Tag id are required")
 	private Integer id;
+
+	@NotBlank(message = "Tag cannot be empty")
 	private String tag;
 }
