94ff3c1c350325e5ed350af296da41a2ef1624bd
andrewtyshkovets <andrew.tyshkovets@gmail.com>
2021-01-18 14:43:27 +0200

    refactor: security tests fixed

--- src/test/java/com/softserveinc/dokazovi/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/softserveinc/dokazovi/security/CustomUserDetailsServiceTest.java
@@ -1,6 +1,9 @@
 package com.softserveinc.dokazovi.security;
 
+import com.softserveinc.dokazovi.entity.ProviderEntity;
+import com.softserveinc.dokazovi.entity.RoleEntity;
 import com.softserveinc.dokazovi.entity.UserEntity;
+import com.softserveinc.dokazovi.entity.enumerations.UserStatus;
 import com.softserveinc.dokazovi.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,9 +12,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,7 +27,13 @@ import static org.mockito.Mockito.when;
 class CustomUserDetailsServiceTest {
 
 	@Mock
+	private UserEntity userEntity;
+
+	@Mock
 	private UserRepository userRepository;
+
+	@Mock
+	private UserPrincipal userPrincipal;
 
 	@InjectMocks
 	private CustomUserDetailsService customUserDetailsService;
@@ -30,9 +42,21 @@ class CustomUserDetailsServiceTest {
 	@Test
 	void loadUserByUsername() {
 		String email = "test@test.com";
+		RoleEntity roleEntity = RoleEntity.builder().id(1).name("ROLE_DOCTOR").build();
+		Set<RoleEntity> roleEntitySet = new HashSet<>();
+		roleEntitySet.add(roleEntity);
 		UserEntity user = UserEntity.builder()
+				.id(1)
+				.status(UserStatus.ACTIVE)
 				.email(email)
+				.roles(roleEntitySet)
+				.enabled(true)
 				.build();
+		ProviderEntity providerEntity = ProviderEntity.builder().id(1).email(email).name("LOCAL").userIdByProvider("1")
+				.user(user).build();
+		Set<ProviderEntity> providerEntities = new HashSet<>();
+		providerEntities.add(providerEntity);
+		user.setUserProviderEntities(providerEntities);
 		when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
 		UserDetails resultUser = customUserDetailsService.loadUserByUsername(email);
 		verify(userRepository, times(1)).findByEmail(email);
@@ -43,10 +67,21 @@ class CustomUserDetailsServiceTest {
 	void loadUserById() {
 		Integer id = 1;
 		String email = "test@test.com";
+		RoleEntity roleEntity = RoleEntity.builder().id(1).name("ROLE_DOCTOR").build();
+		Set<RoleEntity> roleEntitySet = new HashSet<>();
+		roleEntitySet.add(roleEntity);
 		UserEntity userEntity = UserEntity.builder()
-				.id(id)
+				.id(1)
+				.status(UserStatus.ACTIVE)
 				.email(email)
+				.roles(roleEntitySet)
+				.enabled(true)
 				.build();
+		ProviderEntity providerEntity = ProviderEntity.builder().id(1).email(email).name("LOCAL").userIdByProvider("1")
+				.user(userEntity).build();
+		Set<ProviderEntity> providerEntities = new HashSet<>();
+		providerEntities.add(providerEntity);
+		userEntity.setUserProviderEntities(providerEntities);
 		when(userRepository.findById(id))
 				.thenReturn(Optional.of(userEntity));
 		UserDetails resultUser = customUserDetailsService.loadUserById(id);
