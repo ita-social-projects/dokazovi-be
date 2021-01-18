30a5e4821b46295060b1ec15527884bb64b295af
andrewtyshkovets <andrew.tyshkovets@gmail.com>
2021-01-18 15:00:43 +0200

    refactor: checkstyle fixed

--- src/test/java/com/softserveinc/dokazovi/security/CustomUserDetailsServiceTest.java
+++ src/test/java/com/softserveinc/dokazovi/security/CustomUserDetailsServiceTest.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
