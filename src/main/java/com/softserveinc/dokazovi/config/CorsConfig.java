03ffdb47a14fec3a0bcb5a8a80498a2475b8eb06
Ruslan Ponomarenko <panruslan@gmail.com>
2021-01-21 11:50:58 +0200

    refactor: delete unused import

--- src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
+++ src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.softserveinc.dokazovi.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
