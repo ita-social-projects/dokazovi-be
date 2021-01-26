368fa527031d14c1fb9984df6286dd980c4f22a8
brmdm <60578980+brmdm@users.noreply.github.com>
2021-01-26 16:16:34 +0200

    fix: delete unused import

--- src/main/java/com/softserveinc/dokazovi/dto/post/PostSaveFromUserDTO.java
+++ src/main/java/com/softserveinc/dokazovi/dto/post/PostSaveFromUserDTO.java
@@ -12,7 +12,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
