42c3e0a4698ac7a07bb56287b68ccdee26dce1ea
Alex <kp9k999@gmail.com>
2021-03-02 18:33:36 +0200

    Changed * to Heroku project url and localhost url

--- src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
+++ src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000", "https://dokazovi-fe.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
