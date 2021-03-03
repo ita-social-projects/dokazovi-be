fd027e9d76d0ed0e31be539c20dce56077468c31
Alex <kp9k999@gmail.com>
2021-03-03 17:26:05 +0200

    replace string url with local variables

--- src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
+++ src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.softserveinc.dokazovi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,13 @@ public class CorsConfig implements WebMvcConfigurer {
 
     private static final long MAX_AGE_SEC = 3600;
 
+    @Value("${endpoints.cors.allowed-origins}")
+    private String [] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dokazovi-fe.herokuapp.com")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
