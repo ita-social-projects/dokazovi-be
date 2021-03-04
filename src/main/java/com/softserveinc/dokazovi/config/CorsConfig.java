acf2a3262fa24c8c780e91547d5cea1857bb0277
Alex <kp9k999@gmail.com>
2021-03-04 18:41:06 +0200

    add endpoints.cors to test application.properties

--- src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
+++ src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
@@ -2,10 +2,13 @@ package com.softserveinc.dokazovi.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySource("classpath:application.properties")
+
 public class CorsConfig implements WebMvcConfigurer {
 
     private static final long MAX_AGE_SEC = 3600;
