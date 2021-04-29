0ece349faefcaa424f5f09f11ccf2bddd921deee
Maksym Rohachov <romax982@gmail.com>
2021-04-29 19:02:51 +0300

    fix: remove unused import

--- src/main/java/com/softserveinc/dokazovi/controller/OriginController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/OriginController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
