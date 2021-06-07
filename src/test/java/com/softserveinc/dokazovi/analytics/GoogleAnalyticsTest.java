08e9b6c46e609daf2b5b0af7bc9f33c097f7a437
Alex <kp9k999@gmail.com>
2021-06-08 01:44:10 +0300

    delete unused imports

--- src/test/java/com/softserveinc/dokazovi/analytics/GoogleAnalyticsTest.java
+++ src/test/java/com/softserveinc/dokazovi/analytics/GoogleAnalyticsTest.java
@@ -3,16 +3,9 @@ package com.softserveinc.dokazovi.analytics;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GoogleAnalyticsTest {
