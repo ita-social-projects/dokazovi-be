0fba14b37f09a781fff91d9cff8adc5149b028aa
Alex <kp9k999@gmail.com>
2021-06-17 14:05:58 +0300

    delete unused imports

--- src/main/java/com/softserveinc/dokazovi/analytics/GoogleAnalytics.java
+++ src/main/java/com/softserveinc/dokazovi/analytics/GoogleAnalytics.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
