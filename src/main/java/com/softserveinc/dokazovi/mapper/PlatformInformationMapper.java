dd24bbf7b3641a6d2b4685666442ff55c8fac8a5
Stanislav Kucher <kucherstanislav@gmail.com>
2021-07-08 17:19:15 +0300

    The functionality of the PUT endpoint of the PlatformInformationController was changed.

--- src/main/java/com/softserveinc/dokazovi/mapper/PlatformInformationMapper.java
+++ src/main/java/com/softserveinc/dokazovi/mapper/PlatformInformationMapper.java
@@ -2,8 +2,11 @@ package com.softserveinc.dokazovi.mapper;
 
 import com.softserveinc.dokazovi.dto.info.PlatformInformationDTO;
 import com.softserveinc.dokazovi.entity.PlatformInformationEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface PlatformInformationMapper {
@@ -12,6 +15,8 @@ public interface PlatformInformationMapper {
 
 	PlatformInformationEntity toPlatformInformationEntity(PlatformInformationDTO platformInformationDTO);
 
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+			nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 	PlatformInformationEntity updatePlatformInformationEntity(PlatformInformationDTO platformInformationDTO,
 			@MappingTarget PlatformInformationEntity platformInformationEntity);
 }
