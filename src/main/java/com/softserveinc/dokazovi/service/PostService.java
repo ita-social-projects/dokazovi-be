a4728e6e495ee8e05d132aea530063852a7d6bf3
Ihor Zakharko <igor.zaharko@gmail.com>
2021-07-08 22:09:32 +0300

    remove unused import from PostService

--- src/main/java/com/softserveinc/dokazovi/service/PostService.java
+++ src/main/java/com/softserveinc/dokazovi/service/PostService.java
@@ -3,7 +3,6 @@ package com.softserveinc.dokazovi.service;
 import com.softserveinc.dokazovi.dto.post.PostDTO;
 import com.softserveinc.dokazovi.dto.post.PostMainPageDTO;
 import com.softserveinc.dokazovi.dto.post.PostSaveFromUserDTO;
-import com.softserveinc.dokazovi.entity.PostEntity;
 import com.softserveinc.dokazovi.entity.enumerations.PostStatus;
 import com.softserveinc.dokazovi.security.UserPrincipal;
 import org.springframework.data.domain.Page;
