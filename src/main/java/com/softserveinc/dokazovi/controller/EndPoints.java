9b0895a664ddf9dd6a7fb12e97fe86c783e6a619
Ihor Zakharko <igor.zaharko@gmail.com>
2021-07-08 11:14:39 +0300

    add  endpoint POST_GET_IMPORTANT_IMAGE_NULL

--- src/main/java/com/softserveinc/dokazovi/controller/EndPoints.java
+++ src/main/java/com/softserveinc/dokazovi/controller/EndPoints.java
@@ -41,6 +41,7 @@ public final class EndPoints {
 	public static final String POST_TYPES_ALL_TYPES_BY_USER = "/{userId}";
 	public static final String PLATFORM_INFORMATION = "/platform-information";
 	public static final String PLATFORM_INFORMATION_BY_ID = "/{infoId}";
+	public static final String POST_GET_IMPORTANT_IMAGE_NULL = "/get-important-image-null";
 
 	/**
 	 * Method that adds slash after each endpoint while calling
