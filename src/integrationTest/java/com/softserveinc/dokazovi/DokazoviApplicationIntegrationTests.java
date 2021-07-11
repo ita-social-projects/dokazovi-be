e36d6e46019e58e2f8bc69cadc24d5c9e858662c
AlexandrShpyha <saska0811@gmail.com>
2021-07-12 01:35:29 +0300

    first successful integration test run with database

---
+++ src/integrationTest/java/com/softserveinc/dokazovi/DokazoviApplicationIntegrationTests.java
@@ -0,0 +1,11 @@
+package com.softserveinc.dokazovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DokazoviApplicationIntegrationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+//}
