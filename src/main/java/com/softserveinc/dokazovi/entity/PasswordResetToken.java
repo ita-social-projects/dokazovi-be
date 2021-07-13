080509a081e921c02c7f2d45d5090c1549e7fee6
Ihor Zakharko <igor.zaharko@gmail.com>
2021-07-13 12:56:35 +0300

    add some code to PasswordResetToken entity

--- src/main/java/com/softserveinc/dokazovi/entity/PasswordResetToken.java
+++ src/main/java/com/softserveinc/dokazovi/entity/PasswordResetToken.java
@@ -1,12 +1,26 @@
 package com.softserveinc.dokazovi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "password_reset_tokens")
 public class PasswordResetToken {
 
 	private static final int EXPIRATION = 60;
@@ -17,6 +31,8 @@ public class PasswordResetToken {
 
 	private String token;
 
+	@OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+	@JoinColumn(nullable = false, name = "user_id")
 	private UserEntity userEntity;
 
 	private LocalDateTime dateExpiration;
