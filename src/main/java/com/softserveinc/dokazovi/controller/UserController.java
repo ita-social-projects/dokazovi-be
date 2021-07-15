76e0265ad8b65581c98bb215759d387f7fdf6d81
Ihor Zakharko <igor.zaharko@gmail.com>
2021-07-15 16:26:35 +0300

    add fields to the application.properties and Request Header to UserController

--- src/main/java/com/softserveinc/dokazovi/controller/UserController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/UserController.java
@@ -13,17 +13,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 import java.util.UUID;
 
@@ -131,14 +132,13 @@ public class UserController {
 	@PostMapping(USER_RESET_PASSWORD)
 	@ApiOperation(value = "Reset current password")
 	public ResponseEntity<String> resetPassword(
-			HttpServletRequest request, @RequestParam String email) {
+			@RequestHeader HttpHeaders headers, @RequestParam String email) {
 		String message = "If email is correct please find verification letter on your email account!";
 		UserEntity user = userService.findUserEntityByEmail(email);
 		if (user != null) {
-			System.out.println(user.getEmail());
 			String token = UUID.randomUUID().toString();
 			userService.createPasswordResetTokenForUser(user, token);
-			userService.sendEmailWithToken(request.getContextPath(), token, user);
+			userService.sendEmailWithToken(headers.getOrigin(), token, user);
 		}
 		return ResponseEntity.ok().body(message);
 	}
