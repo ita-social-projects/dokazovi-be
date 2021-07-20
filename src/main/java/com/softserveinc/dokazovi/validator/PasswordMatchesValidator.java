9de34b5d87163408b26f45eba746fc3b2e1dbbf4
Ihor Zakharko <igor.zaharko@gmail.com>
2021-07-20 23:58:37 +0300

    get back an remove PASSWORD from name of constant

--- src/main/java/com/softserveinc/dokazovi/validator/PasswordMatchesValidator.java
+++ src/main/java/com/softserveinc/dokazovi/validator/PasswordMatchesValidator.java
@@ -2,7 +2,6 @@ package com.softserveinc.dokazovi.validator;
 
 import com.softserveinc.dokazovi.annotations.PasswordMatches;
 import com.softserveinc.dokazovi.dto.user.UserPasswordDTO;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -10,10 +9,7 @@ import java.util.regex.Pattern;
 
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
 
-	// private static final String PASSWORD_PATTERN = "[^=]{8,24}";
-	@Value("${password.match.pattern}")
-	private String pattern;
-
+	private static final String PATTERN = "[^=]{8,24}";
 
 	@Override
 	public void initialize(PasswordMatches constraintAnnotation) {
@@ -24,7 +20,7 @@ public class PasswordMatchesValidator implements ConstraintValidator<PasswordMat
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		UserPasswordDTO passwordDTO = (UserPasswordDTO) value;
 		return passwordDTO.getNewPassword().equals(passwordDTO.getMatchPassword())
-				&& Pattern.compile(pattern).matcher(passwordDTO.getNewPassword()).matches()
+				&& Pattern.compile(PATTERN).matcher(passwordDTO.getNewPassword()).matches()
 				&& passwordDTO.getToken() != null;
 	}
 }
