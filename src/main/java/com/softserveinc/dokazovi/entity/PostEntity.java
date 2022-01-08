b719d1f89afa376bdf3025beeeda8aff4a8593d0
mvalchyshen <mvalchyshen@gmail.com>
2022-01-09 00:31:32 +0200

    sonarCloud issues passed

--- src/main/java/com/softserveinc/dokazovi/entity/PostEntity.java
+++ src/main/java/com/softserveinc/dokazovi/entity/PostEntity.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
