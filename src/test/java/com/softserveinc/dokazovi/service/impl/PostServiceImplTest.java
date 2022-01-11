90153c5bd6d651d5d14192dbfb3687ead85afe10
mvalchyshen <mvalchyshen@gmail.com>
2022-01-11 14:12:17 +0200

    tests updated

--- src/test/java/com/softserveinc/dokazovi/service/impl/PostServiceImplTest.java
+++ src/test/java/com/softserveinc/dokazovi/service/impl/PostServiceImplTest.java
@@ -60,7 +60,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
