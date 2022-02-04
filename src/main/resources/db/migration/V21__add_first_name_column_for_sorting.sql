72707ca5cd6521b9b5f474814d6c8a7b902dcf93
mvalchyshen <mvalchyshen@gmail.com>
2022-02-04 11:21:27 +0200

    test_integrationtests

--- src/main/resources/db/migration/V21__add_first_name_column_for_sorting.sql
+++ src/main/resources/db/migration/V21__add_first_name_column_for_sorting.sql
@@ -1,4 +1,4 @@
 alter table public.posts
     add column first_name varchar;
 
-update public.posts set first_name = (select first_name from users where posts.author_id = users.user_id)
+update public.posts set first_name = (select first_name from users where posts.author_id = users.user_id);
