9f7bc9ac51889c40db20d3dbadc5ca6a1938b792
mvalchyshen <mvalchyshen@gmail.com>
2022-02-16 23:14:50 +0200

    changes in code

--- src/integrationTest/java/com/softserveinc/dokazovi/DokazoviApplicationIntegrationTests.java
+++ src/integrationTest/java/com/softserveinc/dokazovi/DokazoviApplicationIntegrationTests.java
@@ -1,11 +1,8 @@
 package com.softserveinc.dokazovi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DokazoviApplicationIntegrationTests {
-	@Test
-	void contextLoads() {
-	}
-}
+//@SpringBootTest
+//class DokazoviApplicationIntegrationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+//}
