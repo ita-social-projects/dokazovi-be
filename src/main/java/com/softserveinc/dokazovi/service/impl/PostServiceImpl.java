6755f513d2a1be3511700264584fa4c21485d178
mvalchyshen <75570021+mvalchyshen@users.noreply.github.com>
2022-03-26 18:31:15 +0200

    delete not needed funtionality

--- src/main/java/com/softserveinc/dokazovi/service/impl/PostServiceImpl.java
+++ src/main/java/com/softserveinc/dokazovi/service/impl/PostServiceImpl.java
@@ -33,7 +33,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
