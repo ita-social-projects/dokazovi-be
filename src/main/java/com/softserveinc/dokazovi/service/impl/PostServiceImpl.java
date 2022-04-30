2c1a5bb2450af82d99faf1ce9da86c8573631c19
mvalchyshen <mvalchyshen@gmail.com>
2022-05-01 00:33:58 +0300

    change PostServiceImplTest delete updateStatusTest

--- src/main/java/com/softserveinc/dokazovi/service/impl/PostServiceImpl.java
+++ src/main/java/com/softserveinc/dokazovi/service/impl/PostServiceImpl.java
@@ -72,10 +72,10 @@ public class PostServiceImpl implements PostService {
 				oldEntity,
 				mappedEntity
 		);
+		mappedEntity.setFakeViews(0);
 
 		UserEntity userEntity = userRepository.getOne(userPrincipal.getId());
 		mappedEntity.setImportant(false);
-		mappedEntity.setFakeViews(0);
 		mappedEntity.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
 
 		if (userEntity.getId().equals(postDTO.getAuthorId()) && userPrincipal.getAuthorities().stream()
