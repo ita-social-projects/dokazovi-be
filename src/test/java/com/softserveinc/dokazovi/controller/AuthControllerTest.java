418c4ca785080e10631913c4e51e8e6688310f59
fortamt <fortamt@gmail.com>
2022-07-20 19:00:37 +0300

    - checkstyle

--- src/test/java/com/softserveinc/dokazovi/controller/AuthControllerTest.java
+++ src/test/java/com/softserveinc/dokazovi/controller/AuthControllerTest.java
@@ -77,20 +77,11 @@ class AuthControllerTest {
 		LoginRequest loginRequest = new LoginRequest();
 		loginRequest.setEmail(email);
 		loginRequest.setPassword(password);
-
 		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(
-						loginRequest.getEmail(),
-						loginRequest.getPassword()
-				)
-		);
-
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
 		String token = "950c9760-805e-449c-a966-2d0d5ebd86f4";
-
 		String refreshTokenString = "4a714dd1-a71d-4a29-9327-e172db25a042";
-
 		String uri = AUTH + AUTH_LOGIN;
-
 		UserEntity user = UserEntity.builder()
 				.id(1)
 				.email(email)
@@ -133,10 +124,7 @@ class AuthControllerTest {
 		String refreshTokenString = "4a714dd1-a71d-4a29-9327-e172db25a042";
 		RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
 		refreshTokenRequest.setRefreshToken(refreshTokenString);
-		UserEntity user = UserEntity.builder()
-				.id(1)
-				.enabled(true)
-				.build();
+		UserEntity user = UserEntity.builder().id(1).enabled(true).build();
 		RefreshToken refreshToken = new RefreshToken();
 		refreshToken.setId(1);
 		refreshToken.setToken(refreshTokenString);
