be21339ec1b484d1ab05d4594d073d252d8482e2
mvalchyshen <75570021+mvalchyshen@users.noreply.github.com>
2022-08-16 12:52:07 +0300

    none

--- src/test/java/com/softserveinc/dokazovi/service/impl/AuthorServiceImplTest.java
+++ src/test/java/com/softserveinc/dokazovi/service/impl/AuthorServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.softserveinc.dokazovi.service.impl;
 
 import com.softserveinc.dokazovi.dto.author.AuthorDTO;
+import com.softserveinc.dokazovi.dto.user.UserInstitutionCityDTO;
+import com.softserveinc.dokazovi.dto.user.UserInstitutionDTO;
 import com.softserveinc.dokazovi.entity.DoctorEntity;
 import com.softserveinc.dokazovi.entity.RoleEntity;
 import com.softserveinc.dokazovi.entity.UserEntity;
@@ -26,55 +28,62 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceImplTest {
 
-	@Mock
-	private UserRepository userRepository;
-	@Mock
-	private DoctorRepository doctorRepository;
-	@InjectMocks
-	private AuthorServiceImpl authorService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private DoctorRepository doctorRepository;
+    @InjectMocks
+    private AuthorServiceImpl authorService;
 
-	private UserEntity admin;
+    private UserEntity admin;
 
-	@BeforeEach
-	void init() {
-		Set<RolePermission> rolePermissions = new HashSet<>();
-		rolePermissions.add(RolePermission.CREATE_AUTHOR);
-		rolePermissions.add(RolePermission.UPDATE_AUTHOR);
-		rolePermissions.add(RolePermission.DELETE_AUTHOR);
-		RoleEntity roleEntity = RoleEntity.builder()
-				.id(1)
-				.name("Doctor")
-				.permissions(rolePermissions)
-				.build();
-		admin = UserEntity.builder()
-				.id(1)
-				.email("test@nail.com")
-				.password("12345")
-				.role(roleEntity)
-				.firstName("test")
-				.lastName("test")
-				.avatar("test")
-				.status(UserStatus.ACTIVE)
-				.createdAt(Timestamp.valueOf(LocalDateTime.now()))
-				.doctor(new DoctorEntity())
-				.phone("test")
-				.userProviderEntities(new HashSet<>())
-				.enabled(true)
-				.build();
-	}
+    @BeforeEach
+    void init() {
+        Set<RolePermission> rolePermissions = new HashSet<>();
+        rolePermissions.add(RolePermission.CREATE_AUTHOR);
+        rolePermissions.add(RolePermission.UPDATE_AUTHOR);
+        rolePermissions.add(RolePermission.DELETE_AUTHOR);
+        RoleEntity roleEntity = RoleEntity.builder()
+                .id(1)
+                .name("Doctor")
+                .permissions(rolePermissions)
+                .build();
+        admin = UserEntity.builder()
+                .id(1)
+                .email("test@nail.com")
+                .password("12345")
+                .role(roleEntity)
+                .firstName("test")
+                .lastName("test")
+                .avatar("test")
+                .status(UserStatus.ACTIVE)
+                .createdAt(Timestamp.valueOf(LocalDateTime.now()))
+                .doctor(new DoctorEntity())
+                .phone("test")
+                .userProviderEntities(new HashSet<>())
+                .enabled(true)
+                .build();
+    }
 
-	@Test
-	void createAuthor() {
-		AuthorDTO authorDTO = AuthorDTO.builder()
-				.id(1)
-				.avatar("avatar")
-				.bio("bio")
-				.city(2)
-				.firstName("firstName")
-				.lastName("lastName")
-				.socialNetwork("socialNetwork")
-				.build();
-		assertEquals(1,1);
-	}
+    @Test
+    void createAuthor() {
+        AuthorDTO authorDTO = AuthorDTO.builder()
+                .id(1)
+                .avatar("avatar")
+                .bio("bio")
+                .mainInstitution(UserInstitutionDTO.builder()
+						.city(
+								UserInstitutionCityDTO.builder()
+										.id(2)
+										.name("Lviv")
+										.build()
+						)
+						.build())
+                .firstName("firstName")
+                .lastName("lastName")
+                .socialNetwork("socialNetwork")
+                .build();
+        assertEquals(1, 1);
+    }
 
 }
