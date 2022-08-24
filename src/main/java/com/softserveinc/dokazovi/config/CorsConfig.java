11565d92dc6ce7c6d19ba8384ca9a2d604b98321
fortamt <fortamt@gmail.com>
2022-08-24 07:55:34 +0300

    - settings for cookie

--- src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
+++ src/main/java/com/softserveinc/dokazovi/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package com.softserveinc.dokazovi.config;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +16,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     private static final long MAX_AGE_SEC = 3600;
+
+    @Bean
+    public TomcatContextCustomizer sameSiteCookie() {
+                return context -> {
+                        final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+                        cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+                        context.setCookieProcessor(cookieProcessor);
+                    };
+            }
 
     @Value("${endpoints.cors}")
     private String[]  allowedOrigins;
