e31721f862e4ba74d1648966993ba8b323b4f41f
fortamt <fortamt@gmail.com>
2022-09-11 15:18:29 +0300

    - fix

--- src/main/java/com/softserveinc/dokazovi/controller/CityController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/CityController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +31,7 @@ public class CityController {
                 .body(cityService.findAllCities());
     }
 
-    @PostMapping(CITIES_BY_REGION)
+    @GetMapping(CITIES_BY_REGION)
     @ApiOperation("Get list of cities by region")
     public ResponseEntity<List<CityDTO>> getAllCitiesByRegion(@PathVariable Integer regionId) {
         return ResponseEntity
--- src/main/java/com/softserveinc/dokazovi/controller/RegionController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/RegionController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,7 +39,7 @@ public class RegionController {
 				.body(regionService.findAllRegions());
 	}
 
-	@PostMapping(REGION_BY_CITY)
+	@GetMapping(REGION_BY_CITY)
 	@ApiOperation("Get region by city")
 	public ResponseEntity<RegionDTO> getRegionByCity(@PathVariable Integer cityId) {
 		return ResponseEntity
