ee7531f5f743a611c3a6c4d9a7af246691154363
fortamt <fortamt@gmail.com>
2022-09-14 21:03:12 +0300

    - cookie test

--- src/main/java/com/softserveinc/dokazovi/controller/AuthController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/AuthController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
