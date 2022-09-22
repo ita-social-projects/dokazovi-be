693ada5aa3dfc9ecb1514293818144b69e1049ee
fortamt <fortamt@gmail.com>
2022-09-22 20:20:28 +0300

    - user editing fix

--- src/main/java/com/softserveinc/dokazovi/repositories/CityRepository.java
+++ src/main/java/com/softserveinc/dokazovi/repositories/CityRepository.java
@@ -4,7 +4,10 @@ import com.softserveinc.dokazovi.entity.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<CityEntity, Integer> {
 
+    List<CityEntity> findAllByRegionId(Integer regionId);
 }
