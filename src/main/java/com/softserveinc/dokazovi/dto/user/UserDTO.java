e99e5ed04af7f3cf1ed9e03de7d569dfc7f28330
fortamt <fortamt@gmail.com>
2022-09-22 20:16:48 +0300

    - user editing fix

--- src/main/java/com/softserveinc/dokazovi/dto/user/UserDTO.java
+++ src/main/java/com/softserveinc/dokazovi/dto/user/UserDTO.java
@@ -2,8 +2,10 @@ package com.softserveinc.dokazovi.dto.user;
 
 import com.softserveinc.dokazovi.dto.direction.DirectionDTO;
 import com.softserveinc.dokazovi.dto.region.RegionDTO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.util.Set;
@@ -11,7 +13,10 @@ import java.util.Set;
 /**
  * The User DTO is responsible for passing user data from the server to the client.
  */
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 
 	private Integer id;
