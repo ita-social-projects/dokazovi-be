59dda64f3132d62aed5665980a180b9beec73d59
VadimasikKPI <fhghvhg314@gmail.com>
2023-09-19 11:27:07 +0300

    Create endpoint to get all authors

--- src/main/java/com/softserveinc/dokazovi/controller/AuthorController.java
+++ src/main/java/com/softserveinc/dokazovi/controller/AuthorController.java
@@ -6,16 +6,20 @@ import com.softserveinc.dokazovi.dto.payload.ApiResponseMessage;
 import com.softserveinc.dokazovi.mapper.AuthorMapper;
 import com.softserveinc.dokazovi.security.UserPrincipal;
 import com.softserveinc.dokazovi.service.AuthorService;
+import org.springframework.data.domain.Page;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -80,5 +84,18 @@ public class AuthorController {
         return ResponseEntity
                 .status(200)
                 .body(authorService.delete(authorId, userPrincipal));
+    }
+
+    @GetMapping()
+    @ApiOperation(value = "get authors",
+            authorizations = {@Authorization(value = "Authorization")})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = HttpStatuses.OK, response = ApiResponseMessage.class),
+            @ApiResponse(code = 400, message = HttpStatuses.BAD_REQUEST)
+    })
+    public ResponseEntity<Page<AuthorResponseDTO>> getAllAuthors(@PageableDefault(size = 6) Pageable pageable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(authorService.findAllAuthors(pageable));
     }
 }
