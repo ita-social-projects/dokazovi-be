344c6029bb65a5cf17eda99394ee875380393845
VadimasikKPI <fhghvhg314@gmail.com>
2023-09-21 23:17:08 +0300

    Remove unused imports

--- src/main/java/com/softserveinc/dokazovi/service/LogService.java
+++ src/main/java/com/softserveinc/dokazovi/service/LogService.java
@@ -1,8 +1,6 @@
 package com.softserveinc.dokazovi.service;
 
 import com.softserveinc.dokazovi.dto.log.PostLogDTO;
-import com.softserveinc.dokazovi.entity.LogEntity;
-import com.softserveinc.dokazovi.entity.UserEntity;
 import com.softserveinc.dokazovi.security.UserPrincipal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
