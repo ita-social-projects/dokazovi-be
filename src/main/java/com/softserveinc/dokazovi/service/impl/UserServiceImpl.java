eaa9965612941af227e4acaf7e61c8e86473a406
VadimasikKPI <fhghvhg314@gmail.com>
2023-11-02 20:37:00 +0200

    ya horoshiy, ne biyte mene

--- src/main/java/com/softserveinc/dokazovi/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserveinc/dokazovi/service/impl/UserServiceImpl.java
@@ -45,7 +45,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-    private AuthorMapper authorMapper;
     private final VerificationTokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final PasswordResetTokenService passwordResetTokenService;
